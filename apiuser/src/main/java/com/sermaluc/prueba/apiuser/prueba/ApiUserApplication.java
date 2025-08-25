607ac345a1f8b11a492627089911acf863d45d6b
Diego <dvillegas@cellit.cl>
2025-08-25 17:45:12 -0400

    Estrucutura inicial del proyecto

---
+++ apiuser/src/main/java/com/sermaluc/prueba/apiuser/prueba/ApiUserApplication.java
@@ -0,0 +1,13 @@
+package com.sermaluc.prueba.apiuser.prueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiUserApplication.class, args);
+	}
+
+}
---
+++ apiuser/src/test/java/com/sermaluc/prueba/apiuser/demo/ApiUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sermaluc.prueba.apiuser.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
